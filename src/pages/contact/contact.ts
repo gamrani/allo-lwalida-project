b00c6db37b2f5939f01f466999cebfc8392ccd22
gamranov <gamraniyoussef@gmail.com>
2018-05-21 13:44:01 +0100

    version beta

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,18 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import {ReservationPage} from '../reservation/reservation';
+
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
   value=0;
+  pizza: boolean;
+  panini: boolean;
+
   constructor(public navCtrl: NavController) {
 
   }
-
-  addPrice(a){
-    this.value = this.value +a;
-     alert(this.value);
+  
+  pizzaPrice(){
+    if(this.pizza)  {this.value = this.value +40;}
+    if(!this.pizza && this.value>0) {
+      this.value=this.value-40;
+    }
+  
+  }
+  paniniPrice(){
+    if(this.panini)  {this.value = this.value +50;}
+    if(!this.panini && this.value>0) {
+      this.value=this.value-50;
+    } 
+  }
+  reserver(){
+    if(confirm("Le prix total de votre choix est :  "+this.value)) {
+        this.navCtrl.push(ReservationPage);      
+    }
   }
 }
