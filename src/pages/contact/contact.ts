7594c6180e0ae3428159b39060e8a23baabda0cf
gamranov <gamraniyoussef@gmail.com>
2018-05-21 03:43:34 +0100

    formation ionic

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { HomePage } from '../home/home';
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+  value=0;
   constructor(public navCtrl: NavController) {
 
   }
 
+  addPrice(a){
+    this.value = this.value +a;
+     alert(this.value);
+  }
 }
