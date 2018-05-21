7baa5fa6ab70bd6c759119c3c628bdaaf0408dfc
gamranov <gamraniyoussef@gmail.com>
2018-05-21 18:15:23 +0100

    beta2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { CallNumber } from '@ionic-native/call-number';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  callNumber: CallNumber;
   constructor(public navCtrl: NavController) {
 
   }
   call(){
-    
+    this.callNumber.callNumber("0681674468", true)
+      .then(res => console.log('Launched dialer!', res))
+      .catch(err => console.log('Error launching dialer', err));
    }
    email(){
 
